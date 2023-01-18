4589ae4b792a4433629ccba6dcdf3a081e222911
djszal <szalank.dj@gmail.com>
2023-01-18 15:27:19 -0500

    test test test

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 // This is the Web Server
-const express = require('express');
+const express = require("express");
 const server = express();
 
 // enable cross-origin resource sharing to proxy api requests
 // from localhost:3000 to localhost:4000 in local dev env
-const cors = require('cors');
+const cors = require("cors");
 server.use(cors());
 
 // create logs for everything
-const morgan = require('morgan');
-server.use(morgan('dev'));
+const morgan = require("morgan");
+server.use(morgan("dev"));
 
 // handle application/json requests
 server.use(express.json());
 
 // here's our static files
-const path = require('path');
-server.use(express.static(path.join(__dirname, 'build')));
+const path = require("path");
+server.use(express.static(path.join(__dirname, "build")));
 
 // here's our API
-server.use('/api', require('./api'));
+server.use("/api", require("./api"));
 
 // by default serve up the react app if we don't recognize the route
 server.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // bring in the DB connection
-const { client } = require('./db');
+const { client } = require("./db");
 
 // connect to the server
 const PORT = process.env.PORT || 8080;
@@ -38,9 +38,9 @@ const handle = server.listen(PORT, async () => {
 
   try {
     await client.connect();
-    console.log('Database is open for business!');
+    console.log("Database is open for business!");
   } catch (error) {
-    console.error('Database is closed for repairs!\n', error);
+    console.error("Database is closed for repairs!\n", error);
   }
 });
 
@@ -49,3 +49,5 @@ module.exports = { server, handle };
 
 //TESTING CHANGE
 //TESTING CHANGE 2
+
+//TESTING CHANGE INFINITY
