291b5011a24a29bd674ce16f14ea45a5d09b07dd
Kevin <weberkm5@gmail.com>
2023-01-19 10:22:14 -0600

    completed DB testing, mock data setup and create user DB function

--- db/seed.js
+++ db/seed.js
@@ -1,53 +1,81 @@
 
 const client = require("./client");
+const { getAllUsers, createUser } = require("./models/user");
 
 
-
-async function testDB() {
+async function dropTables() {
   try {
-    // connect the client to the database, finally
-    client.connect();
+  console.log('starting to drop tables....')
 
-    // queries are promises, so we can await them
-    const {rows} = await client.query(`SELECT * FROM users;`);
+    await client.query(`
+    DROP TABLE users;
+    `)
 
-    // for now, logging is a fine way to see what's up
-    console.log(rows);
+    console.log('completed dropping tables!')
   } catch (error) {
-    console.error(error);
-  } finally {
-    // it's important to close out the client connection
-    client.end();
+    console.error(error)
   }
 }
 
-testDB();
+async function createTables() {
+  try {
+    console.log('starting to create tables....')
+
+
+    await client.query(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      email VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL
+    );
+    `);
+    
+    console.log('finished creating tables!');
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+async function createInitialUsers() {
+  const kevin = await createUser({email: 'kevin@email.com', password: 'password1'});
+  const jae = await createUser({email: 'jae@email.com', password: 'password12'});
+  const thomas = await createUser({email: 'thomas@email.com', password: 'password123'});
+  const dylan = await createUser({email: 'dylan@email.com', password: 'password1234'});
+
+}
 
 
 
-// async function buildTables() {
-//   try {
-//     client.connect();
+async function rebuildDB() {
+  try {
+    client.connect();
 
-//     // drop tables in correct order
+    await dropTables();
+    await createTables();
+    await createInitialUsers();
 
-//     // build tables in correct order
-//   } catch (error) {
-//     throw error;
-//   }
-// }
+  } catch (error) {
+    console.error(error)
+  }
+}
 
-// async function populateInitialData() {
-//   try {
-//     // create useful starting data by leveraging your
-//     // Model.method() adapters to seed your db, for example:
-//     // const user1 = await User.createUser({ ...user info goes here... })
-//   } catch (error) {
-//     throw error;
-//   }
-// }
+async function testDB() {
+  try {
+    console.log('starting to test the database....')
+   
+   const users = await getAllUsers();
+   console.log('this is getAllUsers-------->', users);
+   
+   console.log('finsihed testing the database!')
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-// buildTables()
-//   .then(populateInitialData)
-//   .catch(console.error)
-//   .finally(() => client.end());
+rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
+
+
+
