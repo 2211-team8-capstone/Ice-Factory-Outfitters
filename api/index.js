63deb3e224a605cdd99c886aba4039a472e6f0a6
Kevin <weberkm5@gmail.com>
2023-01-19 22:00:16 -0600

    setup server, API routes, completed login and register user

--- api/index.js
+++ api/index.js
@@ -1,19 +1,100 @@
-const apiRouter = require('express').Router();
+const express = require('express');
+const apiRouter = express.Router();
 
-apiRouter.get('/', (req, res, next) => {
-  res.send({
-    message: 'API is under construction!',
-  });
+const jwt = require('jsonwebtoken');
+const { getUserById } = require('../db/models/users');
+const { JWT_SECRET } = process.env;
+
+// assign a user if there is a user to be assigned based on request/authorization
+  // 1. look at auth/req header and if it wasn't set we can't set user just next out of this block
+  // 2. we have have auth, so check if prefix of request starts with bearer, if so then take the token off it and assign it to token. 
+  //    2a. try to verify the token, if successfull try to pull the user from the db by id and assign the user 
+  // 3. there was an error in the header format, try again 
+apiRouter.use(async (req, res, next) => {    
+  const prefix = 'Bearer ';
+  const auth = req.header('Authorization');
+  
+  if (!auth) {
+      console.log("No authorization, moving to next.");
+      next();
+  } else if (auth.startsWith(prefix)) {
+      const token = auth.slice(prefix.length);
+
+      try {
+          const { id } = jwt.verify(token, JWT_SECRET);
+
+          if (id) {
+              req.user = await getUserById(id);
+              // console.log("Authorization successful. Moving to next.");
+              next();
+          } else {
+              // maybe come back and fix this error message
+              next({
+                  name: "LostUserIdError", 
+                  message: "Could not find specified user. Please log out and log back in."
+              });
+          }
+      } catch ({ name, message }) {
+          next({ name, message });
+      }
+  } else {
+      next({
+          name: 'AuthorizationHeaderError',
+          message: `Authorization token must start with ${ prefix }`
+      });
+  }
 });
 
-apiRouter.get('/health', (req, res, next) => {
-  res.send({
-    healthy: true,
-  });
+// check to see if a user is set from func above ^ 
+apiRouter.use((req, res, next) => {
+  if (req.user) {
+      console.log("User is set:", req.user);
+  }
+
+  next();
 });
 
-// place your routers here
+//***********************  ROUTERS BELOW *********************************
 
-// test change
+//router to /api/users
+const usersRouter = require('./users');
+apiRouter.use('/users', usersRouter);
+
+//router to /api/products
+
+//router to /api/purchases
+
+
+//*******************  GENERIC ERROR HANDLER ***********************
+// whenever next function gets called in apiRouter and its children with an object in it (not empty like next()), it comes here and sends that error response 
+apiRouter.use((error, req, res, next) => {
+  res.send({
+    error: "ERROR",
+    name: error.name,
+    message: error.message
+  });
+});
 
 module.exports = apiRouter;
+
+
+// ****************************** boilerplate below ******************
+
+// const apiRouter = require('express').Router();
+
+// apiRouter.get('/', (req, res, next) => {
+//   res.send({
+//     message: 'API is under construction!',
+//   });
+// });
+
+// apiRouter.get('/health', (req, res, next) => {
+//   res.send({
+//     healthy: true,
+//   });
+// });
+
+// // place your routers here
+
+
+// module.exports = apiRouter;
---
+++ db/models/users.js
@@ -0,0 +1,76 @@
+// grab our db client connection to use with our adapters
+const client = require('../client');
+const bcrypt = require('bcrypt');
+
+
+async function createUser ({email, password}) {
+  const SALT_COUNT = 10;
+
+  const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
+
+  try {
+    const {rows: [user]} = await client.query(`
+    INSERT INTO users(email, password)
+    VALUES ($1, $2)
+    ON CONFLICT (email) DO NOTHING
+    RETURNING *;
+    `, [email, hashedPassword]);
+
+    delete user.password;
+
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getAllUsers() {
+  const {rows} = await client.query(`
+    SELECT email, password
+    FROM users;
+  `);
+
+  return rows;
+}
+
+async function getUserById(userId) {
+  try {
+    const { rows: [user] } = await client.query(`
+        SELECT *
+        FROM users
+        WHERE id=$1;
+    `, [userId]);
+
+    // dont return password with user data
+    delete user.password;
+
+    return user;
+  } catch (error) {
+    console.error(error);  
+    throw error;
+  }
+}
+
+async function getUserByEmail(email) {
+  try {
+    const {rows: [user]} = await client.query(`
+      SELECT * 
+      FROM users
+      WHERE email=$1;
+    `, [email]);
+
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+  // add your database adapter fns here
+module.exports = {
+  getAllUsers,
+  createUser, 
+  getUserById,
+  getUserByEmail
+};
+
+
