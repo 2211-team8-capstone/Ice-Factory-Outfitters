671a2cb8c24864c33aaeae02ed98dc0c17c820d3
Jae You <jyou88@gmail.com>
2023-01-24 14:34:58 -0600

    ContactUs EOD

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "../style/ContactUs.css";
-import { getAllProducts } from "../api";
 
 const ContactUs = () => {
 
     return (
         <div className="contact-forms">
+
             <form>
+                <div className='title'>
+                    <h3>Contact Us</h3>
+                </div>
+
                 <div className='contact-first-name'>
                     <label>First Name: </label>
                     <input
@@ -16,12 +20,48 @@ const ContactUs = () => {
                     ></input>
                 </div>
 
-                <label>Last Name: </label>
-                <input
-                    type="text"
-                    placeholder="Last Name"
-                    onChange={(event) => setLastName(event.target.value)}
-                ></input>
+                <div className='contact-last-name'>
+                    <label>Last Name: </label>
+                    <input
+                        type="text"
+                        placeholder="Last Name"
+                        onChange={(event) => setLastName(event.target.value)}
+                    ></input>
+                </div>
+
+                <div className='contact-email'>
+                    <label>Email Address: </label>
+                    <input
+                        type="text"
+                        placeholder="Email Address"
+                        onChange={(event) => setEmail(event.target.value)}
+                    ></input>
+                </div>
+
+                <div className='contact-questions'>
+                    <label>Questions/Comments: </label>
+                    <textarea
+                        id='input-questions'
+                        type="text"
+                        placeholder="Questions/Comments"
+                        onChange={(event) => setQuestions(event.target.value)}
+                    ></textarea>
+                </div>
+
+                <div>
+                    <button
+                        className='contact-submit'
+                        onClick={() => {
+                            submitNewQuestion();
+                        }}
+                        type="button"
+                        name="submit_button"
+                        value="Submit"
+                    >
+                        Submit
+                    </button>
+                </div>
+
             </form>
         </div>
     )
