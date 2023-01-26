714823f19695e7245f74f191682e67642707b2cd
Jae You <jyou88@gmail.com>
2023-01-26 14:27:25 -0600

    Single Product CSS

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -24,29 +24,30 @@ const Profile = (props) => {
   console.log("TTTTTTTTTTTTTTTTTTT", emailTwo, props);
   return (
     <>
-      <div className="user-profile">
-        <h3 className="title">
-          Welcome <span className="display-player playerX"></span>
-        </h3>
-        <div>Email Address: </div>
-        <div>Phone Number: </div>
-        <div>Street Address: </div>
-        <div>Street Address 2: </div>
-        <div>City: </div>
-        <div>State: </div>
-        <div>Zip: </div>
-      </div>
+      <div className="user-profile-container">
+        <div className="user-profile">
+          <h3 className="title">
+            Welcome
+          </h3>
+          <div>Email Address: </div>
+          <div>Phone Number: </div>
+          <div>Street Address: </div>
+          <div>Street Address 2: </div>
+          <div>City: </div>
+          <div>State: </div>
+          <div>Zip: </div>
+        </div>
 
-      <br></br>
+        <br></br>
 
-      <div className="order-history">
-        <h3 className="title">Order History</h3>
-      </div>
+        <div className="order-history">
+          <h3 className="title">Order History</h3>
+        </div>
 
-      <br></br>
+        <br></br>
 
-      <div className="update-profile">
-        {/* <div className="login">
+        <div className="update-profile">
+          {/* <div className="login">
                     <h3>Existing Customers Login</h3>
                     <form
                         className="login-form"
@@ -90,125 +91,126 @@ const Profile = (props) => {
                     </form>
                 </div> */}
 
-        <div className="update">
-          <h3 className="title">Update Profile</h3>
-          <form
-            className="update-form"
-            onSubmit={async (e) => {
-              if (!emailTwo || !passwordTwo) {
-                e.preventDefault();
-                const errorMessage = "Please enter a valid Email and Password";
-                setStateError(errorMessage);
-              } else {
-                try {
+          <div className="update">
+            <h3 className="title">Update Profile</h3>
+            <form
+              className="update-form"
+              onSubmit={async (e) => {
+                if (!emailTwo || !passwordTwo) {
                   e.preventDefault();
+                  const errorMessage = "Please enter a valid Email and Password";
+                  setStateError(errorMessage);
+                } else {
+                  try {
+                    e.preventDefault();
 
-                  localStorage.setItem("email", emailTwo);
-                  const token = await registerUser(emailTwo, passwordTwo);
+                    localStorage.setItem("email", emailTwo);
+                    const token = await registerUser(emailTwo, passwordTwo);
 
-                  props.setToken(token);
-                  localStorage.setItem("token", token);
-                } catch (error) {
-                  console.error(error);
+                    props.setToken(token);
+                    localStorage.setItem("token", token);
+                  } catch (error) {
+                    console.error(error);
+                  }
                 }
-              }
-            }}
-          >
-            <div>
-              <label htmlFor="emailTwo">User Email</label>
-              <input
-                value={emailTwo}
-                type="text"
-                placeholder="Email Address"
-                minLength={3}
-                onChange={(e) => setEmailTwo(e.target.value)}
-              ></input>
-            </div>
-            <div>
-              <label htmlFor="passwordTwo">Password</label>
-              <input
-                value={passwordTwo}
-                type="password"
-                placeholder="Password"
-                minLength={3}
-                onChange={(e) => setPasswordTwo(e.target.value)}
-              ></input>
-            </div>
-            <div>
-              <label htmlFor="nameFirst">First Name</label>
-              <input
-                value={nameFirst}
-                type="first-name"
-                placeholder="First Name"
-                minLength={2}
-                onChange={(e) => setNameFirst(e.target.value)}
-              ></input>
-            </div>
-            <div>
-              <label htmlFor="nameLast">Last Name</label>
-              <input
-                value={nameLast}
-                type="last-name"
-                placeholder="Last Name"
-                minLength={2}
-                onChange={(e) => setNameLast(e.target.value)}
-              ></input>
-            </div>
-            <div>
-              <label htmlFor="addressStreet">Street Address</label>
-              <input
-                value={addressStreet}
-                type="address-street"
-                placeholder="Street Address"
-                minLength={3}
-                onChange={(e) => setAddressStreet(e.target.value)}
-              ></input>
-            </div>
-            <div>
-              <label htmlFor="addressStreet2">Street Address 2</label>
-              <input
-                value={addressStreet2}
-                type="address-street-2"
-                placeholder="Street Address 2"
-                minLength={3}
-                onChange={(e) => setAddressStreet2(e.target.value)}
-              ></input>
-            </div>
-            <div>
-              <label htmlFor="addressCity">City</label>
-              <input
-                value={addressCity}
-                type="address-city"
-                placeholder="City"
-                minLength={3}
-                onChange={(e) => setAddressCity(e.target.value)}
-              ></input>
-            </div>
-            <div>
-              <label htmlFor="addressState">State</label>
-              <input
-                value={addressState}
-                type="address-state"
-                placeholder="State"
-                minLength={3}
-                onChange={(e) => setAddressState(e.target.value)}
-              ></input>
-            </div>
-            <div>
-              <label htmlFor="addressZip">Zip</label>
-              <input
-                value={addressZip}
-                type="address-zip"
-                placeholder="Zip"
-                minLength={3}
-                onChange={(e) => setAddressZip(e.target.value)}
-              ></input>
-            </div>
-            <div>
-              <button type="Update">Update</button>
-            </div>
-            {stateError ? <h3>{stateError}</h3> : ""}
-          </form>
+              }}
+            >
+              <div>
+                <label htmlFor="emailTwo">User Email: </label>
+                <input
+                  value={emailTwo}
+                  type="text"
+                  placeholder="Email Address"
+                  minLength={3}
+                  onChange={(e) => setEmailTwo(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <label htmlFor="passwordTwo">Password: </label>
+                <input
+                  value={passwordTwo}
+                  type="password"
+                  placeholder="Password"
+                  minLength={3}
+                  onChange={(e) => setPasswordTwo(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <label htmlFor="nameFirst">First Name: </label>
+                <input
+                  value={nameFirst}
+                  type="first-name"
+                  placeholder="First Name"
+                  minLength={2}
+                  onChange={(e) => setNameFirst(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <label htmlFor="nameLast">Last Name: </label>
+                <input
+                  value={nameLast}
+                  type="last-name"
+                  placeholder="Last Name"
+                  minLength={2}
+                  onChange={(e) => setNameLast(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <label htmlFor="addressStreet">Street Address: </label>
+                <input
+                  value={addressStreet}
+                  type="address-street"
+                  placeholder="Street Address"
+                  minLength={3}
+                  onChange={(e) => setAddressStreet(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <label htmlFor="addressStreet2">Street Address 2: </label>
+                <input
+                  value={addressStreet2}
+                  type="address-street-2"
+                  placeholder="Street Address 2"
+                  minLength={3}
+                  onChange={(e) => setAddressStreet2(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <label htmlFor="addressCity">City: </label>
+                <input
+                  value={addressCity}
+                  type="address-city"
+                  placeholder="City"
+                  minLength={3}
+                  onChange={(e) => setAddressCity(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <label htmlFor="addressState">State: </label>
+                <input
+                  value={addressState}
+                  type="address-state"
+                  placeholder="State"
+                  minLength={3}
+                  onChange={(e) => setAddressState(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <label htmlFor="addressZip">Zip: </label>
+                <input
+                  value={addressZip}
+                  type="address-zip"
+                  placeholder="Zip"
+                  minLength={3}
+                  onChange={(e) => setAddressZip(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <button type="Update">Update</button>
+              </div>
+              {stateError ? <h3>{stateError}</h3> : ""}
+            </form>
+          </div>
         </div>
       </div>
     </>
