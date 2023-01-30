1ba6c7aeda8d278c17b6cc1cbba64de742338fc2
Jae You <jyou88@gmail.com>
2023-01-30 10:31:20 -0600

    Admin Pages In-Progress

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,6 +11,30 @@ const Footer = (props) => {
                         Contact Us
                     </Link>
                 </div>
+
+                <div className="admin-login"></div>
+                {!props.token ? (
+                    <div className="admin-login-form">
+                        <i className="fa-solid fa-user"></i>
+                        <Link to={"/AdminLogin"} className="foot-links">
+                            Admin Login
+                        </Link>
+                    </div>
+                ) : (
+                    <div className="admin-logged-in">
+                        <div className="admin-profile">
+                            <Link to={"/Admin"} className="foot-links">
+                                Admin
+                            </Link>
+                        </div>
+
+                        <div className="admin-logout" onClick={handleLogout}>
+                            <Link to={"/"} className="foot-links">
+                                Admnin Logout
+                            </Link>
+                        </div>
+                    </div>
+                )}
             </div>
         </>
     );
